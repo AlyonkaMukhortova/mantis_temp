e17ecb0fa2d468740c413c91f1fde2a8f95a0167
vladlenafonin <vladlenafonin.university@yandex.ru>
2023-08-05 01:37:47 +0300

    chore: modify cipher

--- mantis/cipher.py
+++ mantis/cipher.py
@@ -2,44 +2,35 @@ import numpy as np
 import galois
 
 from mantis import constants
+from mantis import math
 from mantis.keys import MantisKeychain
 
 
 class Mantis:
     def __init__(self, nbits: int, nblocks: int, nrounds: int) -> None:
         self._gf = galois.GF(2 ** nbits)
+        self._nbits = nbits
         self._nblocks = nblocks
         self._nrounds = nrounds
-        self._state = None
+        self._key_size = 2 * self._nbits * self._nblocks ** 2
 
-    def encrypt(self, message:str, tweak, key):
-        message = message.join(message.split())
-        self._keychain = MantisKeychain(key)
-        self._state = np.array([])
+    def encrypt(self, message: str, key: str, tweak: str):
+        self._state = self._gf(
+            math.split_int(math.str2int(message), self._nbits)
+                .reshape(self._nblocks, self._nblocks))
 
-    def _round(self, secret, round_constant):
-        self._state = self._gf(self._substitute(self._state.ravel(), constants.S_BOX).reshape((4, 4)))
+        self._keys = MantisKeychain(math.str2int(key), self._key_size, self._nbits)
+
+    def _round(self, secret: galois.typing.Array, round_constant: galois.typing.Array):
+        self._state = self._gf(math.substitute(self._state.ravel(), constants.S_BOX).reshape((4, 4)))
         self._state ^= round_constant
         self._state ^= secret
-        self._state = self._gf(self._permute(self._state.ravel(), constants.P).reshape((4, 4)))
+        self._state = self._gf(math.permute(self._state.ravel(), constants.P).reshape((4, 4)))
         self._state = constants.M.dot(self._state)
 
     def _round_inverse(self, secret, round_constant):
         self._state = constants.M.dot(self._state)
-        self._state = self._gf(self._permute(self._state.ravel(), constants.P_INVERSE).reshape((4, 4)))
+        self._state = self._gf(math.permute(self._state.ravel(), constants.P_INVERSE).reshape((4, 4)))
         self._state ^= secret
         self._state ^= round_constant
-        self._state = self._gf(self._substitute(self._state.ravel(), constants.S_BOX).reshape((4, 4)))
-
-    def _permute(self, matrix: np.ndarray, permutation: np.ndarray):
-        result = np.empty_like(matrix)
-        with np.nditer(permutation) as iterator:
-            for a in iterator:
-                result[iterator.iterindex] = matrix[a]
-        return result
-
-    def _substitute(self, matrix: np.ndarray, lookup_table: np.ndarray):
-        result = np.empty_like(matrix)
-        for i in range(lookup_table.size):
-            result[i] = lookup_table[matrix[i]]
-        return result
+        self._state = self._gf(math.substitute(self._state.ravel(), constants.S_BOX).reshape((4, 4)))
